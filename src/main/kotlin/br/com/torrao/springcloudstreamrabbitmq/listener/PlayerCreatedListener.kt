a06c99b51e3b5ac5cfb8bba97e899eee19b0ccb7
Fabio Pereira <fabio_vihoh@hotmail.com>
2018-06-30 17:33:07 -0300

    adding reactive implementation of a listener with retry and dead letter

--- src/main/kotlin/br/com/torrao/springcloudstreamrabbitmq/listener/PlayerCreatedListener.kt
+++ src/main/kotlin/br/com/torrao/springcloudstreamrabbitmq/listener/PlayerCreatedListener.kt
@@ -1,19 +1,42 @@
 package br.com.torrao.springcloudstreamrabbitmq.listener
 
 import br.com.torrao.springcloudstreamrabbitmq.Sink.Companion.PLAYER_CREATED
+import org.springframework.amqp.core.Message
+import org.springframework.amqp.core.MessageProperties
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.cloud.stream.annotation.Input
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.nio.charset.Charset
 
 @Component
-class PlayerCreatedListener{
+class PlayerCreatedListener(val rabbitTemplate: RabbitTemplate) {
 
     @StreamListener
     fun incoming(@Input(PLAYER_CREATED) messages: Flux<String>) {
-        messages
-                .map { it.toUpperCase() }
-                .subscribe { println("player received $it") }
+        messages.flatMap { message ->
+            Mono.just(message)
+                    .map { handleMessage(it) }
+                    .retry(3)
+                    .onErrorResume { sendToDLQ() }
+        }.log().subscribe()
     }
 
+    private fun handleMessage(it: String?) {
+        print("$it mono --- ")
+        throw IllegalStateException()
+    }
+
+    private fun sendToDLQ(): Mono<Nothing> {
+        return Mono.fromRunnable<Nothing> {
+            println("mono handling error")
+            rabbitTemplate.send(
+                    "DLX",
+                    "$PLAYER_CREATED.social",
+                    Message("lalala".toByteArray(Charset.defaultCharset()), MessageProperties())
+            )
+        }
+    }
 }
